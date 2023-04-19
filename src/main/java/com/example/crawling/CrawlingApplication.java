8c108bc155d4698f2a2ec2a4546b9b2ac0e543ee
김대용 <77915737+withKDY@users.noreply.github.com>
2023-04-19 13:12:50 +0900

    TO

--- src/main/java/com/example/crawling/CrawlingApplication.java
+++ src/main/java/com/example/crawling/CrawlingApplication.java
@@ -11,21 +11,26 @@ public class CrawlingApplication {
 
     public static void main(String[] args) {
         try {
-            String URL = "https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=105";
+            String URL = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=조일+알미늄";
             Document doc = Jsoup.connect(URL).get();
 
-            Elements elements = doc.select(".cluster_text_headline");
+//            Elements elements = doc.select(".cluster_text_headline");
             //헤드라인 제목 추출
-            for (Element element : elements) {
-                System.out.println(element.text());
-            }
+//            for (Element element : elements) {
+//                System.out.println(element.text());
+//            }
 
             //헤드라인 a 태그 추출
-            elements = doc.select(".cluster_text a");
+//            elements = doc.select(".cluster_text a");
 
-            for (Element element : elements) {
-                System.out.println(element.getElementsByAttribute("href").attr("href"));
-            }
+//            for (Element element : elements) {
+//                System.out.println(element.getElementsByAttribute("href").attr("href"));
+//            }
+
+            Elements elements = doc.select(".spt_con strong");
+            int money = 751 * Integer.parseInt(elements.get(0).text().replace(",", ""));
+            System.out.println("현재 보유중인 자산 : " + money + "원");
+
         }catch (Exception e) {
             e.printStackTrace();
         }
